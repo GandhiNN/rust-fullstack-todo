709fa7afe021ce33d49d8634699f0524f5ddd78b
Ngakan Gandhi <ngandhi@pmintl.net>
2026-01-13 14:17:35 +0700

    feat: scaffold init_db pool function

--- backend/src/db.rs
+++ backend/src/db.rs
@@ -1,3 +1,25 @@
-pub async fn init_db() {
-    todo!()
+use sqlx::{SqlitePool, sqlite::SqlitePoolOptions};
+
+pub async fn init_db() -> SqlitePool {
+    let pool = SqlitePoolOptions::new()
+        .max_connections(5)
+        .connect("sqlite::todos.db")
+        .await
+        .expect("Failed to connect to database");
+
+    // Create table if it does not exist
+    sqlx::query(
+        r#"CREATE TABLE IF NOT EXISTS todos (
+                    id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    title TEXT NOT NULL,
+                    completed BOOLEAN NOT NULL DEFAULT 0,
+                    created_at DATETIME NOT NULL)"#,
+    )
+    .execute(&pool)
+    .await
+    .expect("Failed to create table");
+
+    println!("Database initialized");
+
+    pool
 }
